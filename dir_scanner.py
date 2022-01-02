7145bd3112747fce0bccaffa0731f307e91066f4
Dominik Gawlak <dominikpriv6@gmail.com>
2022-01-02 01:40:34 +0100

    compatible with new file_scanner

--- dir_scanner.py
+++ dir_scanner.py
@@ -1,17 +1,18 @@
-#import file_scanner
+from file_scanner import FileScanner
 import os
 
+"""
+TODO: add full path to file and pass to file_scanner
+"""
+
+# temp dir
 dir_linux = "/home/dominik/Desktop/Huawei"
 dir_windows = "C:/Users/gawla/Desktop/documents"
 
+# read all files in dir and pass them to file_sanner
 for file in os.listdir(dir_windows):
-    if file.endswith(".txt"):
-        print("txt \n")
-        #file_scanner.read_txt()
-    elif file.endswith(".docx"):
-        print("doxc \n")
-        #file_scanner.read_doxc()
-    elif file.endswith(".doc"):
-        print("doc \n")
-    elif file.endswith(".odt"):
-        print("odt \n")
+    ext = file.split(".")[1]
+    if ext in ["docx", "doc", "txt"]:
+        fileScanner = FileScanner(ext)
+    else:
+        print("No supported file format")
