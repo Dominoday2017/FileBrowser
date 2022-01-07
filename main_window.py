4248644938e2e7b3e3b293af2e0934aefab06072
Dominik Gawlak <dominikpriv6@gmail.com>
2022-01-07 19:02:41 +0100

    Implemented add keywords functionality. Preparing to merge with file_operations branch

--- main_window.py
+++ main_window.py
@@ -1,8 +1,46 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import QtWidgets, uic
 
+
+"""
+TODO: check if keywords already exists
+"""
+
+
+class GetKeywords(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        uic.loadUi("add_keywords.ui", self)
+        self.keywords = []
+
+        self.cancelBtn.clicked.connect(self.close_window)
+        self.addBtn.clicked.connect(self.add_keyword)
+        self.removeBtn.clicked.connect(self.remove_element)
+        self.saveBtn.clicked.connect(self.save_keywords)
+        self.show()
+
+    def add_keyword(self):
+        keyword = self.keywordEdit.text()
+        if keyword == "":
+            warning = QMessageBox.warning(None, 'Błąd danych wejściowych', "Nie można dodawać pustych elementów do listy.")
+        else:
+            self.keywordsListWidget.addItem(keyword)
+            self.keywordEdit.setText("")
+
+    def remove_element(self):
+        item = self.keywordsListWidget.currentRow()
+        self.keywordsListWidget.takeItem(item)
+
+    def save_keywords(self):
+        for el in range(self.keywordsListWidget.count()):
+            self.keywords.append(self.keywordsListWidget.item(el).text())
+
+        self.close()
+
+    def close_window(self):
+        self.close()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,7 +48,7 @@ class MainWindow(QMainWindow):
         uic.loadUi("main_window.ui", self)
 
         self.pathBtn.clicked.connect(self.get_path) #binding pathBtn button
-        self.searchBtn.clicked.connect(self.get_keywords)
+        self.addBtn.clicked.connect(self.get_keywords)
 
         self.show()
 
@@ -19,14 +57,17 @@ class MainWindow(QMainWindow):
         self.pathEdit.setText(path)
 
     def get_keywords(self):
-        keywords = str(self.keywordsEdit.text())
-        try:
-            keywordsList = keywords.split(" ")
-        except:
-            warning = QMessageBox.warning(None, 'Błąd danych wejściowych', "Upewnij się że oddzieliłeś słowa kluczowe przecinkiem")
-        print(keywordsList,keywords)
+        self.getKeywords = GetKeywords()
+        self.getKeywords.exec_()
 
+        if not self.getKeywords.isVisible():
+            keywordsStr = ""
+            print( keywords := self.getKeywords.keywords)
+            for el in keywords:
+                keywordsStr += el + ", "
 
+            keywordsStr = keywordsStr[0:len(keywordsStr)-2]
+            self.keywordsEdit.setText(keywordsStr)
 
 
 def main():
